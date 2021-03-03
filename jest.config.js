c2765386612ab290801c4caa48caf8e13be47adb
devJovanni <dev@jovanni.services>
2021-03-03 21:21:22 +0200

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: [
+        "<rootDir>/src"
+    ],
+    collectCoverageFrom: [
+        "<rootDir>/src/**/*.ts",
+        "!<rootDir>/src/example/**"
+    ],
+    coverageDirectory: "./coverage/",
+    collectCoverage: true,
+};
